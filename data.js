f5e4a89fbccbf2f305df889843fa885d64bb6579
THE NAMAN GAJJAR <namangajjar013@gmail.com>
2025-12-27 19:51:39 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,8 +1,86 @@
 window.initialDocuments = [
     {
-        "name": "12th.pdf",
-        "size": "599.34 KB",
-        "date": "12/26/2025",
-        "url": "DOCS_PDF/12th.pdf"
+        "name": "Cast_Certificate .pdf",
+        "size": "735.60 KB",
+        "date": "12/27/2025",
+        "url": "DOCS_PDF/Cast_Certificate .pdf"
+    },
+    {
+        "name": "Sem-1_Result.pdf",
+        "size": "3.90 MB",
+        "date": "12/27/2025",
+        "url": "DOCS_PDF/Sem-1_Result.pdf"
+    },
+    {
+        "name": "Election_Card .pdf",
+        "size": "5.55 MB",
+        "date": "12/27/2025",
+        "url": "DOCS_PDF/Election_Card .pdf"
+    },
+    {
+        "name": "Sem-2_Result .pdf",
+        "size": "4.14 MB",
+        "date": "12/27/2025",
+        "url": "DOCS_PDF/Sem-2_Result .pdf"
+    },
+    {
+        "name": "Non_Criminalear.pdf",
+        "size": "1.23 MB",
+        "date": "12/27/2025",
+        "url": "DOCS_PDF/Non_Criminalear.pdf"
+    },
+    {
+        "name": "Passbook.pdf",
+        "size": "1.70 MB",
+        "date": "12/27/2025",
+        "url": "DOCS_PDF/Passbook.pdf"
+    },
+    {
+        "name": "License .pdf",
+        "size": "2.34 MB",
+        "date": "12/27/2025",
+        "url": "DOCS_PDF/License .pdf"
+    },
+    {
+        "name": "12Th_Marksheet .pdf",
+        "size": "7.48 MB",
+        "date": "12/27/2025",
+        "url": "DOCS_PDF/12Th_Marksheet .pdf"
+    },
+    {
+        "name": "Birth_Certificate .pdf",
+        "size": "2.18 MB",
+        "date": "12/27/2025",
+        "url": "DOCS_PDF/Birth_Certificate .pdf"
+    },
+    {
+        "name": "Income_Certificate .pdf",
+        "size": "879.90 KB",
+        "date": "12/27/2025",
+        "url": "DOCS_PDF/Income_Certificate .pdf"
+    },
+    {
+        "name": "Adhar_Card .pdf",
+        "size": "6.54 MB",
+        "date": "12/27/2025",
+        "url": "DOCS_PDF/Adhar_Card .pdf"
+    },
+    {
+        "name": "10TH Marksheet .pdf",
+        "size": "2.05 MB",
+        "date": "12/27/2025",
+        "url": "DOCS_PDF/10TH Marksheet .pdf"
+    },
+    {
+        "name": "LC Of 12Th .pdf",
+        "size": "1.71 MB",
+        "date": "12/27/2025",
+        "url": "DOCS_PDF/LC Of 12Th .pdf"
+    },
+    {
+        "name": "Pan_Card .pdf",
+        "size": "5.76 MB",
+        "date": "12/27/2025",
+        "url": "DOCS_PDF/Pan_Card .pdf"
     }
 ];
